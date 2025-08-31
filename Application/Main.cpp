4101a6f564508125f0fc0ffc1ec0152f8f87b53c
김주영 <98079486+Leusin@users.noreply.github.com>
2025-09-01 03:33:57 +0900

    feat: 타이틀 추가 및 디버그 모드 전처리

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,12 +1,18 @@
+#include "Game/Game.h"
+
+#ifdef _DEBUG
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-
-#include "Game/Game.h"
+#endif
 
 int main()
 {
+#ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	SetConsoleTitleA("ASCII WARS");
 
 	Game game;
 	game.Run();
